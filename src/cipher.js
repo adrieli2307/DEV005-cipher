3726d379af3d22274c6ce189b2d3a088b4a3c2ea
adrieli2307 <aearmijosq1104@gmail.com>
2023-02-12 16:56:42 -0500

    TEST ENCODE Y DECODE

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
+    encode: function(offset, message) {
+        let abecedario = "abcdefghijklmnopqrstuvwxyz";
+        let mensajecifrado = "";
+        for (let i = 0; i < message.length; i++) {
+            let letraactual = message.charAt(i);
+            let posicionactual = abecedario.indexOf(letraactual);
+            let nuevaposicion = posicionactual + offset;
+            let nuevaletra = abecedario.charAt(nuevaposicion % 26);
+            mensajecifrado = mensajecifrado.concat(nuevaletra);
+        }
+        console.log(mensajecifrado);
+    },
+    decode: function(offset, message) {
+        let abecedario = "abcdefghijklmnopqrstuvwxyz";
+        let mensajedescifrado = "";
+        for (let i = 0; i < message.length; i++) {
+            let letraactual = message.charAt(i);
+            let posicionactual = abecedario.indexOf(letraactual);
+            let nuevaposicion = posicionactual - offset;
+            let nuevaletra = abecedario.charAt(nuevaposicion % 26);
+            mensajedescifrado = mensajedescifrado.concat(nuevaletra);
+        }
+        console.log(mensajedescifrado);
+    },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+cipher.encode(2, "teamo");
+cipher.decode(2, "vgcoq");
+
+function descifralo() {
+    let mensaje = prompt("escribe tu mensaje");
+    if (nombre == "") {
+        alert("Debes ingresar tu nombre");
+        return;
+    }
+
+    document.getElementById("mensaje_saludo").innerHTML =
+        "Bienvenido " + nombre + " vamos a jugar";
+}
+document.getElementById("myButton").style.display = "block";
+document.getElementById("mensaje_saludo").style.display = "block";
+document.getElementById("mensaje_bienvenida").style.display = "none";
+
+let botonJugar = document.getElementById("myButton");
+botonJugar.addEventListener("click", descifralo);
