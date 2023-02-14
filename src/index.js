e4677b65599c7493f34ea1a15962e567de91f98c
adrieli2307 <aearmijosq1104@gmail.com>
2023-02-14 17:39:56 -0500

    decode y encode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import cipher from "./cipher.js";
 
-cipher.encode(2, "teamo");
-cipher.decode(2, "vgcoq");
+cipher.encode(3, "teamo");
+cipher.decode(3, "vgcoq");
+
+//CIFRADO MENSAJE
+
+//ATRAER INFORNACION HTML
+let botonCifralo = document.getElementById("buttonEncode ");
+botonCifralo.addEventListener("click", encondeHander);
+let botonDecifralo = document.getElementById("buttonDecode ");
+botonDecifralo.addEventListener("click", decodeHander);
+
+function encondeHander() {
+    console.log("dentro del encode handler");
+    var mensajeIngresado = document.getElementById('mensaje').value;
+    cipher.encode(3, mensajeIngresado);
+}
+
+function decodeHander() {
+    console.log("dentro del encode handler");
+    var mensajeIngresado = document.getElementById('mensaje').value;
+    cipher.decode(3, mensajeIngresado);
+}
