0fa58065422dff5d625301dbfc9d5239ec579c19
adrieli2307 <aearmijosq1104@gmail.com>
2023-02-22 21:46:44 -0500

    Test

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import cipher from "./cipher.js";
 
 //ATRAER INFORMACION HTML
 function encondeHandler() {
-    let mensajeIngresado = document.getElementById('mensaje').value;
-    let mensajeCifrado = cipher.encode(3, mensajeIngresado);
-    document.getElementById('mensaje').value = mensajeCifrado;
-    console.log(mensajeCifrado);
-    // desactivar boton
+  let mensajeIngresado = document.getElementById('mensaje').value;
+  let mensajeCifrado = cipher.encode(3, mensajeIngresado);
+  document.getElementById('mensaje').value = mensajeCifrado;
+  console.log(mensajeCifrado);
+  // desactivar boton
+  botonCifralo.disabled = true;
+  botonDecifralo.disabled = false;
 }
 
 function decodeHandler() {
-    let mensajeIngresado = document.getElementById('mensaje').value;
-    let mensajedescifrado = cipher.decode(3, mensajeIngresado);
-    document.getElementById('mensaje').value = mensajedescifrado;
-    console.log(mensajedescifrado);
+  let mensajeIngresado = document.getElementById('mensaje').value;
+  let mensajedescifrado = cipher.decode(3, mensajeIngresado);
+  document.getElementById('mensaje').value = mensajedescifrado;
+  console.log(mensajedescifrado);
+  botonCifralo.disabled = false;
+  botonDecifralo.disabled = true;
 }
 
 let botonCifralo = document.getElementById("buttonEncode");
