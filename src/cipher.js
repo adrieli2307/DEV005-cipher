45307206bfc556588e32a9398556e35c14b05f2c
adrieli2307 <aearmijosq1104@gmail.com>
2023-02-22 22:15:44 -0500

    Correcciones test

--- src/cipher.js
+++ src/cipher.js
@@ -1,37 +1,36 @@
 const cipher = {
-    encode: function(offset, message) {
-        let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let mensajecifrado = "";
-        for (let i = 0; i < message.length; i++) {
-            let letraactual = message.charAt(i);
-            let nuevaletra = "";
+  encode: function (offset, message) {
+    let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let mensajecifrado = "";
+    for (let i = 0; i < message.length; i++) {
+      const letraactual = message.charAt(i);
+      let nuevaletra = "";
+      // Revisar si es minuscula
+      if (letraactual == letraactual.toLowerCase()) {
+        abecedario = abecedario.toLowerCase();
+      } else {
+        abecedario = abecedario.toUpperCase();
+      }
 
-            // Revisar si es minuscula
-            if (letraactual == letraactual.toLowerCase()) {
-                abecedario = abecedario.toLowerCase();
-            } else {
-                abecedario = abecedario.toUpperCase();
-            }
+      // Revisar si existe en el vocabulario
+      if (abecedario.indexOf(letraactual) == -1) {
+        nuevaletra = letraactual;
+      } else {
+        const posicionactual = abecedario.indexOf(letraactual);
+        const nuevaposicion = posicionactual + offset;
+        nuevaletra = abecedario.charAt(nuevaposicion % 26);
+      }
+      mensajecifrado = mensajecifrado.concat(nuevaletra);
+    }
 
-            // Revisar si existe en el vocabulario
-            if (abecedario.indexOf(letraactual) == -1) {
-                nuevaletra = letraactual;
-            } else {
-                let posicionactual = abecedario.indexOf(letraactual);
-                let nuevaposicion = posicionactual + offset;
-                nuevaletra = abecedario.charAt(nuevaposicion % 26);
-            }
-            mensajecifrado = mensajecifrado.concat(nuevaletra);
-        }
-
-        console.log(mensajecifrado);
-        return mensajecifrado;
-    },
-    decode: function(offset, message) {
-        let mensajedescifrado = this.encode(-offset, message)
-        console.log(mensajedescifrado);
-        return mensajedescifrado;
-    },
+    console.log(mensajecifrado);
+    return mensajecifrado;
+  },
+  decode: function (offset, message) {
+    const mensajedescifrado = this.encode(-offset, message)
+    console.log(mensajedescifrado);
+    return mensajedescifrado;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import cipher from "./cipher.js";
 
 //ATRAER INFORMACION HTML
 function encondeHandler() {
-  let mensajeIngresado = document.getElementById('mensaje').value;
-  let mensajeCifrado = cipher.encode(3, mensajeIngresado);
+  const mensajeIngresado = document.getElementById('mensaje').value;
+  const mensajeCifrado = cipher.encode(3, mensajeIngresado);
   document.getElementById('mensaje').value = mensajeCifrado;
   console.log(mensajeCifrado);
   // desactivar boton
@@ -12,16 +12,16 @@ function encondeHandler() {
 }
 
 function decodeHandler() {
-  let mensajeIngresado = document.getElementById('mensaje').value;
-  let mensajedescifrado = cipher.decode(3, mensajeIngresado);
+  const mensajeIngresado = document.getElementById('mensaje').value;
+  const mensajedescifrado = cipher.decode(3, mensajeIngresado);
   document.getElementById('mensaje').value = mensajedescifrado;
   console.log(mensajedescifrado);
   botonCifralo.disabled = false;
   botonDecifralo.disabled = true;
 }
 
-let botonCifralo = document.getElementById("buttonEncode");
+const botonCifralo = document.getElementById("buttonEncode");
 botonCifralo.addEventListener("click", encondeHandler);
 
-let botonDecifralo = document.getElementById("buttonDecode");
+const botonDecifralo = document.getElementById("buttonDecode");
 botonDecifralo.addEventListener("click", decodeHandler);
