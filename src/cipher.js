d53b8329fc6c02f449755311a1491702c47e2eca
adrieli2307 <aearmijosq1104@gmail.com>
2023-02-23 21:51:49 -0500

    Añadir type check

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,36 @@
 const cipher = {
   encode: function (offset, message) {
-    let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let mensajecifrado = "";
-    for (let i = 0; i < message.length; i++) {
-      const letraactual = message.charAt(i);
-      let nuevaletra = "";
-      // Revisar si es minuscula
-      if (letraactual === letraactual.toLowerCase()) {
-        abecedario = abecedario.toLowerCase();
-      } else {
-        abecedario = abecedario.toUpperCase();
-      }
 
-      // Revisar si existe en el vocabulario
-      if (abecedario.indexOf(letraactual) === -1) {
-        nuevaletra = letraactual;
-      } else {
-        const posicionactual = abecedario.indexOf(letraactual);
-        const nuevaposicion = posicionactual + offset;
-        nuevaletra = abecedario.charAt(nuevaposicion % 26);
+    if (typeof offset === 'number' &&
+      typeof message === 'string') {
+      let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let mensajecifrado = "";
+      for (let i = 0; i < message.length; i++) {
+        const letraactual = message.charAt(i);
+        let nuevaletra = "";
+
+        // Revisar si es minuscula
+        if (letraactual === letraactual.toLowerCase()) {
+          abecedario = abecedario.toLowerCase();
+        } else {
+          abecedario = abecedario.toUpperCase();
+        }
+
+        // Revisar si existe en el vocabulario
+        if (abecedario.indexOf(letraactual) === -1) {
+          nuevaletra = letraactual;
+        } else {
+          const posicionactual = abecedario.indexOf(letraactual);
+          const nuevaposicion = posicionactual + offset;
+          nuevaletra = abecedario.charAt(nuevaposicion % 26);
+        }
+        mensajecifrado = mensajecifrado.concat(nuevaletra);
       }
-      mensajecifrado = mensajecifrado.concat(nuevaletra);
+      return mensajecifrado;
+    } else {
+      throw new TypeError()
     }
-    return mensajecifrado;
+
   },
   decode: function (offset, message) {
     const mensajedescifrado = this.encode(-offset, message)
